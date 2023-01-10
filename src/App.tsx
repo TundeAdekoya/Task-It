dfa5b68f2ccdb6f41e84f05b4008425fd739689c
Adekoya Tunde <adekoyagbolahan101@gmail.com>
2023-01-10 17:52:07 +0100

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,83 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-
-let name:string;
-let age: number | string;
-let isStudent: boolean;
-let hobbies: string[];
-let role: [number, string];
-
-let printName: (name:string) => never;
-
-// function printName(name:string){
-//   console.log(name)
-// }
-
-// printName('tunde')
-
-// type Person = {
-//   name: string,
-//   age?: number
-// }
-
-// let person:Person = {
-//   name: 'tunde',
-//   age: 4
-// }
-
-// let groupOfPerson: Person[] = [
-//   {
-//     name: 'tunde',
-//     age: 4
-//   }, 
-//   {
-//     name: 'tunde',
-//     age: 4
-//   }, 
-//   {
-//     name: 'tunde',
-//     age: 4
-//   }
-// ]
-
-interface Person{
-  name: string,
-  age?: number
-}
-
-// let you:Person ={
-//   name: 'dsd',
-//   age: 1212,
-//   a: 'sdsds',
-//   b: 1212
-// }
-
-type X = Person & {
-  a: string,
-  b: number
-}
-
-
-
-
-
-
-type Y = X & {
-  c: string,
-  d: number
-}
-
-
-
-
-
-
-
+import InputField from './Components/InputField';
+import TodoList from './Components/TodoList';
+import { Todo } from './model';
 
 const App:React.FC= () => {
+
+   const [todo, setTodo] = useState<string>('');
+   const [todos, setTodos] = useState<Todo[]>([])
+  //  console.log(todo)
+
+  const handleAdd = (e: React.FormEvent) => {
+      e.preventDefault()
+      if (todo){
+        setTodos([...todos,{id:Date.now(), todo, isDone:false}])
+        setTodo('')
+      }
+  }
+
+  console.log(todos)
+
   return (
     <div className="App">
       <span className="heading">Task It</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      <TodoList todos={todos} setTodos={setTodos} />
+      {/* {todos.map((t) => (
+        <li>{t.todo}</li> 
+      ))} */}
     </div>
   );
 }
